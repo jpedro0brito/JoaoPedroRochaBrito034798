756482e50a914178e6a40ba906911b6b384216c1
JPedro Brito <jpedro0brito@gmail.com>
2026-01-24 16:31:26 -0400

    Commit Inicial do projeto

---
+++ src/main/java/com/tocadiscojp/seplagbackend/SeplagBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tocadiscojp.seplagbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeplagBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeplagBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tocadiscojp/seplagbackend/SeplagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tocadiscojp.seplagbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeplagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

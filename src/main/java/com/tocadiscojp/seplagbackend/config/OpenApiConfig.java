0ecc25b45f9701a8533878c57077b6588ae68fb1
JPedro Brito <jpedro0brito@gmail.com>
2026-02-04 17:52:34 -0400

    Ref Implementa autenticação JWT.
    refatora configuração do OpenAPI para autenticação JWT.
    Inclui configuração para aumentar o tamanho do cabeçalho HTTP.

--- src/main/java/com/tocadiscojp/seplagbackend/config/OpenApiConfig.java
+++ src/main/java/com/tocadiscojp/seplagbackend/config/OpenApiConfig.java
@@ -11,24 +11,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("API de Gerenciamento Musical - Seplag Backend 2026")
-                        .description("### Instruções de Acesso\n" +
-                                "Para testar os endpoints protegidos:\n" +
-                                "1. Realize o login no endpoint **/auth/login**\n" +
-                                "2. Copie o **accessToken** recebido\n" +
-                                "3. Clique no botão **Authorize** ao lado\n" +
-                                "4. Cole o token no campo **Value**"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                .name("bearerAuth")
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .description(
-                                        "Insira apenas o token JWT. O prefixo 'Bearer ' será adicionado automaticamente.")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("API de Gerenciamento Musical - Seplag Backend 2026")
+                                                .description("### Instruções de Acesso\n" +
+                                                                "Para testar os endpoints protegidos:\n" +
+                                                                "1. Realize o login no endpoint **/auth/login**\n" +
+                                                                "2. Copie o **accessToken** recebido\n" +
+                                                                "3. Clique no botão **Authorize** ao lado\n" +
+                                                                "4. Cole o token no campo **Value**"))
+                                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement()
+                                                .addList("bearerAuth"))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                                                .name("bearerAuth")
+                                                                .type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")
+                                                                .description("Insira apenas o token JWT. O prefixo 'Bearer ' será adicionado automaticamente.")));
+        }
 }
